162952ed3e9e86cfeee14ee197db0074c99fb26e
Volodymyr Petrovskyi <volodymyr.petrovskyi@gmail.com>
2023-06-30 19:42:53 +0300

    v0.2 used Handler instead Thread

--- app/src/main/java/training/android/taxiapp/SplashScreenActivity.java
+++ app/src/main/java/training/android/taxiapp/SplashScreenActivity.java
@@ -5,31 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.os.Handler;
+import android.os.Looper;
+
+
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    sleep(1000);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    startActivity(new Intent(SplashScreenActivity.this, ChooseModeActivity.class));
-                }
-            }
-        };
-        thread.start();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(SplashScreenActivity.this, ChooseModeActivity.class));
+            finish(); // Optional: Close the splash activity if needed
+        }, 2000); // Specify the delay time in milliseconds (e.g., 2000 for 2 seconds)
+
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        finish();
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
     }
 }
